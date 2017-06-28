2b6fbf6e0ed84e9e6e62bb212871bff8eaf94710
kevinqy001 <qiaoyi003@126.com>
2017-06-28 14:02:49 +0800

    http ajax

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const router = require('koa-router')();
 const app = new Koa();
 const service = require('./service/webAppService');
 const koa_static = require('koa-static-server');
-const views = require('co-views');
-const render = views('./view', {
-  map: {html: 'ejs'}
-});
+const bodyParser = require('koa-bodyparser');
+app.use(bodyParser());
+const views = require('koa-views');
+const path = require('path');
+app.use(views(path.join(__dirname, './view'), {
+  extension: 'ejs'
+}));
 
 app.use(koa_static({
   rootDir: './static/',
@@ -20,7 +23,9 @@ router.get('/router_test', async (ctx) => {
   ctx.body = "Try to reach Koa-router";
 });
 router.get('/ejs_test', async (ctx) => {
-  ctx.body = await render('test', {title: 'Hello_ejs'});
+  await ctx.render('test', {
+    title: 'Hello EJS',
+  })
 });
 router.get('/data_test', async (ctx) => {
   ctx.body = await service.getTestData();
@@ -28,23 +33,25 @@ router.get('/data_test', async (ctx) => {
 router.get('/ajax/index', async (ctx) => {
   ctx.body = await service.getIndexData();
 });
-router.get('/ajax/search', async (ctx, next) => {
+router.get('/ajax/search', async (ctx) => {
   const qs = require('querystring');
-  let params = qs.parse(ctx.req._parsedUrl.query);
+  let params = ctx.request.query;
   let start = params.start;
   let end = params.end;
   let keyword = params.keyword;
-  ctx.body = await service.getSearchData(start, end, keyword);
+  let data = {
+    start,
+    end,
+    s: keyword
+  };
+  let content = qs.stringify(data);
+  ctx.body = await service.getSearchData(content);
+
 });
-
-
-
-
-
 
 
 app.use(router.routes());
 
-app.listen(3001);
-console.log('Koa Server is running!');
+app.listen(3002);
+console.log('Koa Server is running at localhost:3002!');
 
--- service/webAppService.js
+++ service/webAppService.js
@@ -5,35 +5,28 @@ module.exports.getTestData = function () {
 module.exports.getIndexData = function () {
   return fs.readFileSync('./mock/index.json', 'UTF-8')
 };
-module.exports.getSearchData = (start, end, keyword) => {
-  return (callback) => {
+module.exports.getSearchData = (content) => {
+  return new Promise((resolve) => {
     const http = require('http');
-    const qs = require('querystring');
-    let data = {
-      start,
-      end,
-      s: keyword,
-    };
-    console.log(data);
-    let content = qs.stringify(data);
     let http_request = {
       hostname: 'dushu.xiaomi.com',
       port: 80,
       path: '/store/v0/lib/query/onebox?' + content
     };
-    const req_obj = http.request(http_request, (_res) => {
+    const req_obj = http.request(http_request, (res) => {
       let content = '';
-      _res.setEncoding('utf8');
-      _res.on('data', (chunk) => {
-          content += chunk;
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        content += chunk;
       });
-      _res.on('end', () => {
-        callback(null, content)
+      res.on('end', () => {
+        resolve(content)
       })
     });
     req_obj.on('error', () => {
 
     });
     req_obj.end();
-  }
+   });
+
 };
