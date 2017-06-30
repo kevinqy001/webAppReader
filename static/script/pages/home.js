60a4834c4fccd32468512f325c9e28fb3878c801
kevinqy001 <qiaoyi003@126.com>
2017-06-30 17:31:15 +0800

    main page data/ajax & tab change

--- static/script/pages/home.js
+++ static/script/pages/home.js
@@ -1,16 +1,25 @@
-new Vue({
-  data () {
-    return {
-      headerSearchShow: false,
-      vHeaderSearchShow: false
+axios.get('/ajax/home').then((res)=>{
+  new Vue({
+    data () {
+      return {
+        headerSearchShow: false,
+        vHeaderSearchShow: false,
+        recommendList: res.data.items[2].data.data,
+        freeList: res.data.items[5].data.data,
+        curTab: true,
+      }
+    },
+    methods: {
+      headerSearchCon () {
+        this.headerSearchShow = !this.headerSearchShow;
+        setTimeout(() => {
+          this.vHeaderSearchShow = !this.vHeaderSearchShow
+        },200)
+      },
+      changeTab (way) {
+        this.curTab = way !== 1;
+      }
     }
-  },
-  methods: {
-    headerSearchCon () {
-      this.headerSearchShow = !this.headerSearchShow;
-      setTimeout(() => {
-         this.vHeaderSearchShow = !this.vHeaderSearchShow
-      },200)
-    }
-  }
-}).$mount('#root');
+  }).$mount('#root');
+});
+
